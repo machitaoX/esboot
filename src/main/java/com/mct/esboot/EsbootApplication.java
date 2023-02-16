9ded7860d3b6217be269ea452f8c8377e537858a
machitao <1035937382@qq.com>
2023-02-17 01:55:37 +0800

    init

---
+++ src/main/java/com/mct/esboot/EsbootApplication.java
@@ -0,0 +1,13 @@
+package com.mct.esboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mct/esboot/EsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mct.esboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
